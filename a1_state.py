17a1ff94aad296a521d942c130bfc7f83f4cc7ba
Luca323 <uht23etu@uea.ac.uk>
2025-10-01 16:47:40 +0100

    dimensions

--- a1_state.py
+++ a1_state.py
@@ -1,9 +1,12 @@
 class State:
     def __init__(self, grid):
         self.grid = grid
+        m = len(grid)
+        n = len(grid[0])
+        self.dimensions = (m,n) #Records dimension of grid
         
     def __str__(self):
-        return "\n\n".join("   ".join(str(x) for x in row) for row in self.grid)
+        return f"Dimensions: {self.dimensions}\n" + "\n\n".join("   ".join(str(x) for x in row) for row in self.grid)
     
 examp = [[0,0,1,2], [1, 1, 2,0], [0, 2, 0, 1]]
 
