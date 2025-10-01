aeae8c144d4165774729b4cd863e0c8755a529f2
Luca323 <uht23etu@uea.ac.uk>
2025-10-01 16:34:49 +0100

    Update a1_state.py

--- a1_state.py
+++ a1_state.py
@@ -1,1 +1,11 @@
+class State:
+    def __init__(self, grid):
+        self.grid = grid
+        
+    def __str__(self):
+        return "\n".join(" ".join(str(x) for x in row) for row in self.grid)
+    
+examp = [[0,0,1,2], [1, 1, 2,0], [0, 2, 0, 1]]
 
+s = State(examp)
+print(s)
