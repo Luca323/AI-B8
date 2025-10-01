e56d5a2e4167e5e77e2c37a96723680cb8ddac83
Luca323 <uht23etu@uea.ac.uk>
2025-10-01 17:09:08 +0100

    Update a1_state.py

--- a1_state.py
+++ a1_state.py
@@ -1,14 +1,20 @@
 class State:
     def __init__(self, grid):
         self.grid = grid
-        m = len(grid)
-        n = len(grid[0])
-        self.dimensions = (m,n) #Records dimension of grid
+        self.dimensions = (len(grid),len(grid[0])) #Records dimension of grid
         
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Dimensions: {self.dimensions}\n" + "\n\n".join("   ".join(str(x) for x in row) for row in self.grid)
     
-examp = [[0,0,1,2], [1, 1, 2,0], [0, 2, 0, 1]]
+    def move(self, pos: list) -> 'State':
+        assert(len(pos)=2)
+        i,j = pos[0],pos[1]
+        
+        
+        pass
+    
+    
+examp = [[0,0,1,2], [0, 0, 2,0], [0, 2, 0, 1]]
 
-s = State(examp)
+s = State(grid=examp)
 print(s)
