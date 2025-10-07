37720c506e740a978e07414a0936f338a8df0bb4
Luca323 <uht23etu@uea.ac.uk>
2025-10-07 15:20:23 +0100

    Revert "Update a1_state.py"
    This reverts commit 69b569ccc599ec3bafaf26e350f4216f1bfcaa49.

--- a1_state.py
+++ a1_state.py
@@ -47,61 +47,14 @@ class State:
               for i in range(self.dimensions[0])
               for j in range(self.dimensions[1])
               if self.grid[i][j] != 0] #Collect coords of all points
-<<<<<<< Updated upstream
 
         if not coords:
             return 0 #Throw 0 if no regions exist
-=======
-
-        if not coords:
-            return 0 #Throw 0 if no regions exist
-    
-        coords = set(coords)
-        unvisited = set(coords)
-        regions = 0
-    
-        #all possible neighbor directions
-        neighbors = [
-            (-1, -1), (-1, 0), (-1, 1),
-            (0, -1),(0, 1), (1, -1),  (1, 0), (1, 1)
-            ]
-    
-        while unvisited:
-            regions += 1
-            #start from one point
-            to_check = [unvisited.pop()]
-            checked = set(to_check)
-    
-            i = 0
-            while i < len(to_check):
-                r, c = to_check[i]
-                for dr, dc in neighbors:
-                    n = (r + dr, c + dc)
-                    if n in unvisited:
-                        unvisited.remove(n)
-                        to_check.append(n)
-                i += 1
-    
-        return regions
-    
-    def num_Hingers(self) -> int:
-        h = 0
-        for s in self.moves():
-            if s.numRegions != self.numRegions():
-                h += 1
-        return h
-    
-        
-
-def tester() -> None:    
-    examp = [[1,0,1,2], [0, 0, 0,0], [0, 2, 0, 1]]
->>>>>>> Stashed changes
     
         coords = set(coords) #open
         unvisited = set(coords) #Closed
         regions = 0
     
-<<<<<<< Updated upstream
         #all possible neighbor directions
         neighbors = [
             (-1, -1), (-1, 0), (-1, 1),
@@ -152,18 +105,6 @@ def tester() -> None:
         
         
 
-=======
-    s = State(grid=examp)
-    print(s, "\n") 
-    
-    '''
-    test.append(s.moves)
-    for m in s.moves():
-        print(m, "\n")
-    '''
-    print(f'Number of Regions: {s.numRegions()}')
-    print(f'Number of hinger cells: {s.num_Hingers()}')
->>>>>>> Stashed changes
         
 if __name__ == "__main__":
     tester()
