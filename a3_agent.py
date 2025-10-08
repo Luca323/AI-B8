de54e9de5362553deff6d8c4090454531d74b313
Luca323 <uht23etu@uea.ac.uk>
2025-10-08 14:52:32 +0100

    stuff

---
+++ a3_agent.py
@@ -0,0 +1,36 @@
+"""
+Hinger Project
+Coursework 001
+
+@Authors: B8 (100415489, 100426892, 100437079)
+"""
+from a1_state import State
+from a2_path import *
+
+class Agent:
+    def __init__(self, name: str,  size: tuple):
+        self.name = name
+        self.size = size
+        self.modes = ['alphabeta', 'minimax']
+        
+    def __str__(self):
+        return f"Agent {self.name}"
+    
+    def move(self, st: State, mode=None) -> State:
+        moves = []
+        for m in st.moves():
+            moves.append(m) #Cache all available moves
+            
+        if len(moves) > 0:
+            
+            if not mode:
+                return moves[0] #Makes first available move
+            
+        return None
+    
+    def win(self, st: State) -> bool:
+        if st.num_Hingers() > 0:
+            
+            return True
+        
+        return False
