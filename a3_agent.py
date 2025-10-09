b54f2d1a507b64ab70f460eaf86d260d172d860d
Luca323 <uht23etu@uea.ac.uk>
2025-10-09 15:39:52 +0100

    bullshit

--- a3_agent.py
+++ a3_agent.py
@@ -6,6 +6,7 @@ Coursework 001
 """
 from a1_state import State
 from a2_path import *
+from math import inf
 
 class Agent:
     def __init__(self, name: str,  size: tuple):
@@ -16,23 +17,73 @@ class Agent:
     def __str__(self):
         return f"Agent {self.name}"
     
+    def evaluate(self, st: State, parent_reg: int) -> int:
+        if st.numRegions() > parent_reg:
+            return 9999 #Adds large bias for taking hinger cells
+        
+        active_cells = sum(sum(1 for val in row if val != 0) for row in st.grid)
+        return active_cells
+    
+    def is_terminal(self, st: State) -> bool:
+        return st.num_Hingers() > 0
+            
+    
+    def minimax(self, st: State, depth: int, maximising: bool, parent_reg=None) -> tuple:
+        r = st.numRegions()
+        
+        if depth == 0 or self.is_terminal(st):
+            if parent_reg:
+                score = self.evaluate(st, parent_reg)
+            else:
+                score = self.evaluate(st, r)
+            return score
+        
+        best_state = None
+        
+        if maximising:
+            max_eval = -inf
+            for nxt_st in st.moves():
+                eval_score ,_ = self.minimax(nxt_st, depth-1, False, r)
+                
+                if eval_score > max_eval:
+                    max_eval, best_state = eval_score, nxt_st
+                    
+            return max_eval
+        
+        else:
+            min_eval = inf
+            for nxt_st in st.moves():
+                eval_score ,_ = self.minimax(nxt_st, depth-1, True, r)
+                
+                if eval_score < min_eval:
+                    min_eval, best_state = eval_score, nxt_st
+                    
+            return min_eval
+            
+            
     def move(self, st: State, mode=None) -> State:
         moves = []
+        best_move = None
+        best_score = -inf
         for m in st.moves():
             moves.append(m) #Cache all available moves
             
         if len(moves) > 0:
             
             if not mode:
-                return moves[0] #Makes first available move
+                best_move = moves[0] #Makes first available move
             
             if mode == 'alphabeta':
                 pass
             
             if mode == 'minimax':
-                pass
+                for m in moves:
+                    score = self.minimax(m, depth=3, maximising=False)
+                    if score > best_score:
+                        best_score = score
+                        best_move = m
             
-        return None
+        return best_move
     
     
     
@@ -42,3 +93,36 @@ class Agent:
             return True
         
         return False
+    
+def agent_tester():
+    grid = [[0,1,1,1],[0,0,1,1],[1,1,1,0],[0,0,1,0]]
+    sa = State(grid)
+    
+    
+    
+    minimax_agent = Agent("mm Tester", sa.dimensions)
+    
+    print(minimax_agent.move(sa, mode='minimax'))
+
+if __name__ == "__main__":
+    agent_tester()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
