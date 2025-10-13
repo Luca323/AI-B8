91fe9cd9d37c62c86feac7536f4c100a1ea8cc56
abzozovskij <abzozovskij@gmail.com>
2025-10-13 15:35:28 +0100

    grid randomiser added and fixed illegal move error

--- a4_game.py
+++ a4_game.py
@@ -7,7 +7,7 @@ Coursework 001
 from a1_state import State
 from a3_agent import Agent
 import time
-import sys
+import random
 
 class Player(Agent):
     def __init__(self, name="Player", size: tuple = (0, 0)):
@@ -28,9 +28,12 @@ def play(st: State, agentA: Agent, agentB: Agent)->None:
         player_play = False
     else:
         print("You are now playing the game.")
-        player_play = True
-    
-    
+        time.sleep(0.5)
+        player_name = input("Input your name \n")
+        if player_name:
+            player.name = player_name
+            player_play = True
+        
     while True:
         if player_play:
             if player_turn:
@@ -38,6 +41,7 @@ def play(st: State, agentA: Agent, agentB: Agent)->None:
                 player_move = playermove(st)
                 if player_move == (-1, -1):
                     print("Illegal move. Cannot remove from a 0. You lose.")
+                    time.sleep(0.5)
                     print(f"{agentA.name} wins the game.")
                     return
                 else:
@@ -47,6 +51,7 @@ def play(st: State, agentA: Agent, agentB: Agent)->None:
                 current_agent = agentA
                 next_state = current_agent.move(st)
             print(f"{current_agent.name}'s move:\n" + str(next_state))
+            time.sleep(1.5)
         else:
             current_agent = agentA if agent_turn == 'A' else agentB
             
@@ -77,25 +82,32 @@ def playermove(st: State) -> tuple:
     player_moved = False
     start_time = time.time()
     print("Your turn")
+    time.sleep(1)
     while player_moved == False:
-        row = int(input("Enter Column:\n"))
-        column = int(input("Enter Row:\n"))
-        if st.grid[row - 1][column - 1] < 1:
+        column = int(input("Enter Column:\n"))
+        row = int(input("Enter Row:\n"))
+        if st.grid[column - 1][row - 1] < 1:
             return -1, -1
         
-        print(f"Your move is ({row}), ({column})")
+        print(f"Your move is ({column}), ({row})")
         player_moved = True
     return column - 1, row - 1
 
-
+def randomisegrid(height: int, width: int):
+    grid = []
+    for x in range(height):
+        gridvals = []
+        for x in range(width):
+            gridval = random.randint(1, 3)
+            gridvals.append(gridval)
+        grid.append(gridvals)
+    return grid
 
 def playtest():
-    grid = [[1,3,1,1],[1,0,2,1],[1,1,1,3],[4,1,1,1]]
+    grid = randomisegrid(4,4)
     start_state = State(grid=grid)
-    
     agentA = Agent("Jotaro Kujo", start_state.dimensions)
     agentB = Agent("Dio Brando", start_state.dimensions)
-    
     play(start_state, agentA, agentB)
     
 if __name__ == "__main__":
